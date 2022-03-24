7fc110b3d2501d4272a7d9495bff270befc59e3d
Patricia <waltraute@hotmail.com>
2022-03-24 12:27:51 +0100

    fet

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,18 +1,19 @@
-const inquirer = require('inquirer');
-require ('colors');
+const inquirer = require("inquirer");
+require("colors");
 
-
-console.log("=======================".green);
-console.log("Seleccioni una opció:".yellow);
-console.log("=======================\n".green);
-
-const inquirerMenu = [{
-  type: 'rawlist',
-  message: red("Benvinguda a l'aplicació App_Tasks. Què vols fer? Escull una opció:"),
-  name: 'aplicacio',
-  choices: [
-      "Apple",
-      "Pear",
-      "Banana"
-  ]
-}];
+const inquirerMenu = [
+  {
+    type: "rawlist",
+    name: "aplicacio",
+    message: red(
+      "Benvinguda a l'aplicació App_Tasks. Què vols fer? Escull una opció:"
+    ),
+    choices: ["verd", "blau", "groc"],
+  },
+].then((respostes) => {
+  console.log("Resposta: ", respostes);
+  console.log("=======================".green),
+    console.log("Seleccioni una opció:".yellow),
+    console.log("=======================\n".green);
+  console.log(`Responstes 2: ${respostes}`);
+});
